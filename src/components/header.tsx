2403272892ab40d072fa1352096ca2f223620ea3
abba710 <Abbasaifer3@gmail.com>
2025-11-22 13:43:31 +0500

    update my second extension ad

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,6 @@ import adsimg from '@/assets/Prompt.png'
 function UserHeader() {
   return (
     <div className="relative flex items-center justify-between w-full h-[40px] px-4 bg-white/10 rounded-[16px]">
-      {/* Plan status indicator */}
       <a
         className="flex items-center gap-2 hover:scale-105 transition-transform"
         target="_blank"
@@ -11,7 +10,7 @@ function UserHeader() {
         }
       >
         <img src={adsimg} alt="AI Icon" className="w-5 h-5 rounded-full" />
-        <p className="text-white text-xs font-medium cursor-pointer">
+        <p className="text-white text-[14px] font-medium cursor-pointer">
           {chrome.i18n.getMessage('app_ads_title')}
         </p>
       </a>
