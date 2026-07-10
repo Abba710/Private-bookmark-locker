d134f84c83d857431bda6747a18760e12810498c
Abba710 <abbasaifer3@gmail.com>
2026-07-10 17:52:24 +0500

    fix(reddit link): fix reddit link

--- src/components/notifications/notificationsModal.tsx
+++ src/components/notifications/notificationsModal.tsx
@@ -150,7 +150,7 @@ function RedditPostCard({ postUrl }: { postUrl: string }) {
  */
 export default function ChangelogDialog() {
   const { notificationOpen, setNotificationOpen } = useNotificationDialogStore()
-  const redditPostUrl = "https://www.reddit.com/u/Sad-Bed-3125/s/rNccs9emFL"
+  const redditPostUrl = "https://www.reddit.com/user/Sad-Bed-3125/comments/1u7a1k3/locker_private_bookmark_locker_v210_add_side/"
   if (!notificationOpen) return null
 
   return (
